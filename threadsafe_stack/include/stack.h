4fba79a701ca55f6f2a111e0934ad982e73b309c
havebridge <meyl0r@yandex.com>
2023-05-12 19:59:04 +0300

    first commit

---
+++ threadsafe_stack/include/stack.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+void test()
+{
+	std::cout << "hello\n";
+}
---
+++ threadsafe_stack/src/main.cpp
@@ -0,0 +1,11 @@
+#include "../include/stack.h"
+
+int main(int argc, char** argv)
+{
+	(void)argc;
+	(void)argv;
+
+	test();
+
+	return 0;
+}
