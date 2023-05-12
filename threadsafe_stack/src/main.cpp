ea48a2f1e1d05733fcfc395dd2f20f8b4b4e6c75
havebridge <meyl0r@yandex.com>
2023-05-12 22:01:35 +0300

    shit test

--- threadsafe_stack/src/main.cpp
+++ threadsafe_stack/src/main.cpp
@@ -1,11 +1,52 @@
-#include "../include/stack.h"
+#include "../include/threadsafe_stack.h"
+
+void testPush(Threadsafe_stack<int>& st)
+{
+	std::vector<std::thread> workers;
+	for (int i = 0; i < 5; i++) {
+		workers.push_back(std::thread([&]()
+			{
+				st.push(i);
+			}));
+	}
+	
+	std::for_each(workers.begin(), workers.end(), [](std::thread& t)
+		{
+			t.join();
+		});
+
+	st.print();
+
+}
+
+void testPop(Threadsafe_stack<int>& st)
+{
+	std::vector<std::thread> workers;
+
+	for (int i = 0; i < 5; i++) {
+		workers.push_back(std::thread([&]()
+			{
+				st.pop();
+			}));
+	}
+	std::for_each(workers.begin(), workers.end(), [](std::thread& t)
+		{
+			t.join();
+		});
+
+	st.print();
+}
+
 
 int main(int argc, char** argv)
 {
 	(void)argc;
 	(void)argv;
+	Threadsafe_stack<int> st;
 
-	test();
+
+	testPush(st);
+	testPop(st);
 
 	return 0;
 }
