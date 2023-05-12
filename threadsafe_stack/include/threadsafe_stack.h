51c593f4a593770343ce3da56ef83155f0f7053f
havebridge <meyl0r@yandex.com>
2023-05-12 22:01:08 +0300

    Thread-safe stack

---
+++ threadsafe_stack/include/threadsafe_stack.h
@@ -0,0 +1,53 @@
+#include "stack.h"
+
+#include <thread>
+#include <mutex>
+
+#include <vector>
+
+#include <algorithm>
+
+template<typename T>
+class Threadsafe_stack
+{
+private:
+	Stack<T> data;
+	mutable std::mutex mt;
+public:
+	Threadsafe_stack() : data(10) {}
+
+	Threadsafe_stack(const Threadsafe_stack& other)
+	{
+		std::lock_guard<std::mutex> lock(mt);
+		data = other.data;
+	}
+
+	Threadsafe_stack& operator=(const Threadsafe_stack&) = delete;
+	~Threadsafe_stack() = default;
+public:
+	void push(T new_data)
+	{
+		std::lock_guard<std::mutex> lock(mt);
+		data.push(std::move(new_data));
+	}
+
+	std::shared_ptr<T> pop()
+	{
+		std::lock_guard<std::mutex> lock(mt);
+		std::shared_ptr<T> res(std::make_shared<T>(std::move(data.peek())));
+		data.pop();
+		return res;
+	}
+
+	bool isEmpty() const
+	{
+		std::lock_guard<std::mutex> lock(mt);
+		return data.isEmpty();
+	}
+
+	void print() const
+	{
+		std::lock_guard<std::mutex> lock(mt);
+		data.print();
+	}
+};
