e8c6b96acd9bc6de4617184615cc2af08ee55c3f
havebridge <meyl0r@yandex.com>
2023-05-12 20:47:55 +0300

    stack

--- threadsafe_stack/include/stack.h
+++ threadsafe_stack/include/stack.h
@@ -1,8 +1,91 @@
 #pragma once
 
 #include <iostream>
+#include <cassert>
 
-void test()
+template <typename T>
+class Stack
 {
-	std::cout << "hello\n";
-}
+private:
+	T* data;
+	T top;
+	uint8_t size;
+public:
+	Stack() : data(new T[1024]), top(0), size(1024) {}
+
+	Stack(int size) : data(new T[size]), top(0), size(size) {}
+
+	Stack(const Stack& other)
+	{
+		assert(other.data != nullptr);
+
+		data = new T[size];
+		size = other.size;
+
+		for (int i = 0; i != size; ++i)
+		{
+			data[i] = other.data[i];
+		}
+	}
+
+	Stack& operator=(const Stack& other)
+	{
+		if (&other == this)
+		{
+			return *this;
+		}
+
+		delete[] data;
+
+		if (other.data)
+		{
+			data = new T[size];
+
+			for (int i = 0; i != size; ++i)
+			{
+				data[i] = other.data[i];
+			}
+		}
+		else
+		{
+			data = nullptr;
+		}
+	}
+
+	~Stack()
+	{
+		delete[] data;
+	}
+public:
+	void push(const T new_data)
+	{
+		assert(top <= size);
+		data[top++] = new_data;
+	}
+
+	T pop()
+	{
+		assert(top != 0);
+		return data[top--];
+	}
+
+	T peek() const
+	{
+		return top;
+	}
+
+	bool isEmpty() const
+	{
+		return top == 0;
+	}
+
+	void print() const
+	{
+		std::cout << "Stack: \n";
+		for (int i = 0; i != top; ++i)
+		{
+			std::cout << data[i] << '\n';
+		}
+		std::cout << '\n';
+	}
+};
