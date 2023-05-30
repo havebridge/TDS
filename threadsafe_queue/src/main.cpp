3674faec2532381e090b76581d7f5cef8d8ca5e7
havebridge <meyl0r@yandex.com>
2023-05-30 22:10:09 +0300

    some tests

--- threadsafe_queue/src/main.cpp
+++ threadsafe_queue/src/main.cpp
@@ -5,7 +5,15 @@ int main(int argc, char** argv)
 	(void)argc;
 	(void)argv;
 
-	std::cout << "Hey\n";
+	Queue<int> a;
+	a.Enqueue(2);
+	a.Enqueue(1);
+	a.Enqueue(51);
+	a.Enqueue(4);
 
+	a.print();
+
+	a.Dequeue();
+	a.print();
 	return 0;
 }
