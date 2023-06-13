114fe3017decaabd7e63faac25cd44b8db76b72e
havebridge <meyl0r@yandex.com>
2023-06-13 21:17:48 +0300

    queue tests

--- threadsafe_queue/test/queue.h
+++ threadsafe_queue/test/queue.h
@@ -1,5 +1,34 @@
 #pragma once
 
-#include "../include/queue.h"
+#include "../include/thread_pool.h"
 
-namespace
+using namespace HQ;
+
+namespace QueueTest
+{
+	template<typename T>
+	inline void testPush(Queue<T>& a)
+	{
+		a.Enqueue(2);
+		a.Enqueue(1);
+		a.Enqueue(51);
+		a.Enqueue(4);
+		std::cout << '\n';
+	}
+
+	template<typename T>
+	inline void testPrint(Queue<T>& a)
+	{
+		a.print();
+	}
+
+
+	template<typename T>
+	inline void testPop(Queue<T>& a)
+	{
+		a.Dequeue();
+		a.Dequeue();
+		a.Dequeue();
+		std::cout << '\n';
+	}
+}
